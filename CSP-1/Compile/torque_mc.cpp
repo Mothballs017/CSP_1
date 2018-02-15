65517a281c675e4d9e6c34084f2a8df260483288
mxc6641 <mxc6641@ECTET-1350-12.main.ad.rit.edu>
2018-02-15 12:46:05 -0500

    finished classwork, finish hw at home

---
+++ CSP-1/Compile/torque_mc.cpp
@@ -0,0 +1,21 @@
+/*********************************************************************************/
+/*** Title: torque_mc.cpp											 		   ***/
+/*** Course: Computational Problem Solving CPET-121							   ***/
+/*** Developer: Matthew Chu													   ***/
+/*** Data: Feb 15, 2018                                                        ***/
+/*** Description: Calculating torque							               ***/
+/*********************************************************************************/
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <stdio.h>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	float T,r,F,theta;
+}
+
+
