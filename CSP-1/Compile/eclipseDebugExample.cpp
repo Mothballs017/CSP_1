193bde078f0a21a45b2ae2dfc1bd017ab1aba3bb
Batman <Batman@Batcomputer.wireless.rit.edu>
2018-03-22 12:33:39 -0400

    classwork

--- CSP-1/Compile/eclipseDebugExample.cpp
+++ CSP-1/Compile/eclipseDebugExample.cpp
@@ -10,13 +10,13 @@
 using namespace std;
 int main(void){
 	const int NUMELSX=2,NUMELSY=4;
-	int square[NUMELSX][NUMELSY]={1,2,3,4,5,6,7,8},t,s,cont;
+	int square[NUMELSX][NUMELSY]={1,2,3,4,5,6,7,8},t,s;
 	cout<<"\n"<<"The computer will create a 2 X 4 initialized"<<endl;
 	cout<<"array of the variable square[t][s]."<<"\n"<<endl;
 	cout<<"The array created is:"<<"\n"<<endl;
 	for (t=0;t<NUMELSX;++t){
 		for (s=0;s<NUMELSY;++s)
-			cout<<setw(4)<<square[s][t];
+			cout<<setw(4)<<square[t][s];
 		cout<<"\n"<<endl;
 	}
 	return(0);
