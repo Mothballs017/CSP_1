1f928b55f25b11ac4e05e94ab5d21fb69abc147c
Batman <Batman@Batcomputer.wireless.rit.edu>
2018-03-22 12:59:29 -0400

    fuck this

---
+++ CSP-1/Compile/electrical_eng_mc.cpp
@@ -0,0 +1,32 @@
+/*********************************************************************************/
+/*** Title: electrical_eng_mc.cpp									 		   ***/
+/*** Course: Computational Problem Solving CPET-121							   ***/
+/*** Developer: Matthew Chu													   ***/
+/*** Data: March 22, 2018                                                      ***/
+/*** Description: 3 Arrays in Electrical Eng					               ***/
+/*********************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+//double* Calc_Volts(double[],double[],double[],const int);
+//
+//int main(void){
+//	const int LISTED=10;
+//	double volts[LISTED];
+//	double current[LISTED]={10.62,14.89,13.21,16.55,18.62,9.47,6.58,18.32,12.15,3.98};
+//	double resistance[LISTED]={4,8.5,6,7.35,9,15.3,3,5.4,2.9,4.8,};
+//	volts= Calc_Volts(volts,current, resistance,LISTED);
+//	for(int i=0; i<LISTED; i++){
+//		cout << volts[i] << endl;
+//	}
+//	return 0;
+//}
+//
+//double* Calc_Volts(double[] volt, double curr[],double resist[],const int num){
+//	for(int i=0; i<num; i++){
+//		volt[i]=curr[i]*resist[i];
+//	}
+//	return volt;
+//}
