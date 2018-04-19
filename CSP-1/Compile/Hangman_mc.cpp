ba7e733a38d76d709063f8f338a732b83b3ad1bb
Batman <Batman@Batcomputer.wireless.rit.edu>
2018-04-18 22:50:10 -0400

    for class

---
+++ CSP-1/Compile/Hangman_mc.cpp
@@ -0,0 +1,42 @@
+/*********************************************************************************/
+/*** Title: Hangman_mc.cpp													   ***/
+/*** Course: Computational Problem Solving CPET-121							   ***/
+/*** Developer: Matthew Chu													   ***/
+/*** Data: April 19, 2018                                                      ***/
+/*** Description: Final Project: Hangman			                       	   ***/
+/*********************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+//Functions:
+//select word
+//draw
+//check letter
+//check gamestate
+
+string word;
+
+void SelectWord(){
+
+}
+
+
+
+int main(void)
+{
+	//string1 = wrong guess string
+	//string2 = display guess string
+	//int guess number
+	//int wrong guess number
+	//string gallow[9]={" ","----+","|","O"," |","\|","\|/","/","/ \"};
+	string gallows[9];
+	return 0;
+}
+/*********************************************************************************/
